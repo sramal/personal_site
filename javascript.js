d08113f0fa547d7b8998a6ba93fdba389278e229
Suresh Ramalingam <sramal3202@hotmail.com>
2023-09-15 19:47:41 -0400

    Added responsive nav bar

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+const toggleMenu = document.getElementsByClassName("toggle-menu")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleMenu.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+});
